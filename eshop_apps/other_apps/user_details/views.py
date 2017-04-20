f1ffc5f39edb5854a95280da690d0bdb26dcd37f
Navis <nikavr@192.168.1.4>
2017-04-20 18:27:31 +0300

    History Complete

--- eshop_apps/other_apps/user_details/views.py
+++ eshop_apps/other_apps/user_details/views.py
@@ -4,11 +4,30 @@ from django.shortcuts import render
 from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from ..shopping_cart.models import Reservations
+from ..homeapp.models import Movies
+from cart.cart import Cart
+import datetime
 
 @login_required
 def get_user_history(request):
+    current_cart = Cart(request)
+    myCart = Cart.get_cart_details(current_cart)
     user_id = request.user.id
-    orders = Reservations.objects.get(user_id=user_id)
-    return render(request,'user_history.html',{
-        'orders': orders
+    orders = Reservations.objects.filter(user_id=user_id)
+    full_results = []
+    for order in orders:
+        temp_movies = order.shopping_cart
+        temp = {'order_id': order.id, 'movies': temp_movies.split(","), 'created': order.created}
+        full_results.append(temp)
+    total = []
+    for result in full_results:
+        for movie in result['movies']:
+            movie_details = Movies.objects.get(pk=movie)
+            temp = {'order_id': result['order_id'], 'title': movie_details.title, 'movie_id': movie}
+            total.append(temp)
+    return render(request, 'user_history.html', {
+        'orders': orders,
+        'movies': total,
+        'total_items': myCart['total_items'],
+        'total_price': myCart['total_price'],
     })
